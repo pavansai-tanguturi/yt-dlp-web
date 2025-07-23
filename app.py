ca6e2f3022106506ddb509267f6e62eb515a63c8
pavansai-tanguturi <svntpavansai@gmail.com>
2025-07-23 06:33:31 +0530

    Enable debug mode for Flask application in production

--- app.py
+++ app.py
@@ -250,7 +250,5 @@ def serve_static(filename):
     return send_from_directory('static', filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
